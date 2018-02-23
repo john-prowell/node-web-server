18f364961a4e3f0b2f19fd89de9bb85cb6c8d694
jprowell <dtroxx@gmail.com>
2018-02-22 19:56:07 -0800

    updates

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials'); // partial files
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server log.');
+    }
+  });
+  next();
+});
+
+// app.use((req, res) => {
+//   res.render('maintenance.hbs');
+// });
+
 app.use(express.static(__dirname + '/public')); // static files
 
 hbs.registerHelper('getCurrentYear', () => { // helper function for current year
@@ -34,6 +54,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000!');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`)
 });
